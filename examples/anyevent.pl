601d7f3d3df23509500d14b5a667a6e02c89203b
Marc Mims <marc@questright.com>
2016-11-24 06:53:30 -0800

    Add example

---
+++ examples/anyevent.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/env perl
+use 5.12.1;
+use strictures 2;
+
+use AnyEvent;
+use Twitter::API::AnyEvent;
+
+my $api = Twitter::API::AnyEvent->new(
+    consumer_key        => $ENV{CONSUMER_KEY},
+    consumer_secret     => $ENV{CONSUMER_SECRET},
+    access_token        => $ENV{ACCESS_TOKEN},
+    access_token_secret => $ENV{ACCESS_TOKEN_SECRET},
+);
+
+my $cv = AE::cv;
+
+$api->get('account/verify_credentials', sub {
+    my ( $error, $r ) = @_;
+
+    $cv->croak($error) if $error;
+    say "$$r{screen_name} is authorized";
+    $cv->send;
+});
+
+$cv->recv;
+
